b87b6a8a9bc48014cb3c02e7172f5575647360ca
Timothy Oladapo <Oladapo Timo Timothy>
2022-09-09 18:45:19 -0700

    task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,9 @@ int main(void)
 	int ch;
 
 	printf("Print all uppercase letters followed :\n");
-for (ch = 'A'; ch <= 'Z'; ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar (ch);
-for (ch = 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar (ch);
-putchar ('\n');
 	return (0);
 }
