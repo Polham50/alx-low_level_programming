4f89dd955ee79cdcadae90354e80c03138e96e22
Timothy Oladapo <Oladapo Timo Timothy>
2022-09-09 19:15:51 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for num (num = 0; num < 10; num++)
+		print("%d", num);
+	print("\n");
+	return (0);
+}
