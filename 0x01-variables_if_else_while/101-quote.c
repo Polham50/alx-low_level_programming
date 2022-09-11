445eb53fea84decb7f7ec492c116d6252eacc273
Timothy Oladapo <Oladapo Timo Timothy>
2022-09-11 12:04:01 -0700

    task

--- 0x01-variables_if_else_while/101-quote.c
+++ 0x01-variables_if_else_while/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - ENtry point
+ * main - entry point
  *
- * Description: Prints out the last part of a qoute in the standard
+ * Description: Prints out
  *
  * Return: Always 0 (success)
  */
@@ -12,3 +12,5 @@ int main(void)
 	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
+
+
