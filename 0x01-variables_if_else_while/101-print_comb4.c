9f77f0781d54174518633a6749df7da1f4d90444
Timothy Oladapo <Oladapo Timo Timothy>
2022-09-12 04:54:16 -0700

    task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,27 +16,27 @@ int main(void)
 	{
 		while (j <= '8')
 		{
-			while (k <= '9')
+			while (l <= '9')
 			{
-				if (i< j && j < k)
+				if (i < j && j < k)
 				{
 					putchar(i);
 					putchar(j);
 					putchar(k);
-					if (!(i == '7' && j == '8' && k == '9'))
-					{	
+					if (!(! == '7' && j == '8' && k == '9'))
+					{
 						putchar(',');
 						putchar(' ');
-					{
+					}
 				}
 				k++;
 			}
 			k = '0';
-				j++;
+			j++;
 		}
 		j = '0';
+		i++;
 	}
 	putchar('\n');
 	return (0);
 }
-
