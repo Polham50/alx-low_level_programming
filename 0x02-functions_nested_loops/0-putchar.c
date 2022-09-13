dadfce131b1601c9fcedeaec7cc0a0268ec0db4b
Timothy Oladapo <Oladapo Timo Timothy>
2022-09-13 05:57:50 -0700

    task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 
 /**
  * main - entry point
@@ -10,6 +10,14 @@
  */
 int main(void)
 {
-	putchar("_putchar\n");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
