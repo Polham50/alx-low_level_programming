ebc2818c576811284279928f200e686fe85a73d7
Timothy Oladapo <Oladapo Timo Timothy>
2022-09-13 05:47:10 -0700

    task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print _putchar
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	putchar("_putchar\n");
+	return (0);
+}
