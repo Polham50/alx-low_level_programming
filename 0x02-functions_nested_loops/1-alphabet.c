e02842d1701e8d3a0c61fdcd69b8d9fff53c377b
Timothy Oladapo <Oladapo Timo Timothy>
2022-09-13 14:48:58 -0700

    task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
  * main - entry point
@@ -12,7 +12,8 @@ void print_alphabet(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
+	{
 		_putchar(letter);
-	_putchar('\n');
-	return 0;
+	}	
+_putchar('\n');
 }
