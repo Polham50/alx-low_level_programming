0200661144ff1d9b5888d669e431d0ca9188868c
Timothy Oladapo <90522458+Polham50@users.noreply.github.com>
2022-09-19 23:23:38 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,19 @@
+#ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: main.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x04-pointers_arrays_strings directory.
+ */
+
+#include <stdlib.h>
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
+size_t _strlen(const char *str);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -12,4 +23,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-
+#endif
