9e2200b7c5962139123b7642e09076a76d8da09f
Timothy Oladapo <90522458+Polham50@users.noreply.github.com>
2022-09-27 00:16:28 +0100

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x06-pointers_arrays_strings directory.
+ */
+
+#include <stddef.h>
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
