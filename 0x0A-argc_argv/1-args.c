0ed8aa1c55b33a53751bceac4c8babd68b0a77e1
Timothy Oladapo <90522458+Polham50@users.noreply.github.com>
2022-09-30 23:55:50 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,13 @@
-has
+#include <stdio.h>
+/**
+ * main - a program that prints a number of arguments passed into it
+ * @args: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int args, char *argv[])
+{
+	printf("%d\n", args - 1);
+	(void)argv;
+	return (0);
+}
