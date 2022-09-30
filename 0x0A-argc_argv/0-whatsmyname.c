6087e73e80ed97561d536217a969d57d4a99be76
Timothy Oladapo <90522458+Polham50@users.noreply.github.com>
2022-09-30 23:55:25 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,16 @@
-jahd
+#include <stdio.h>
+/**
+ * main - a program that prints its name, followed by a new line.
+ * if you you rename the program, it will print the new name, without having to
+ * compile it again
+ * you should not remove the path before the name of the program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
