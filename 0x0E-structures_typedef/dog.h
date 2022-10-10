bd890514dcbf45c664b12c5042f895b988d9d73a
Timothy Oladapo <90522458+Polham50@users.noreply.github.com>
2022-10-11 00:31:22 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,24 @@
-#include <stdio.h>
-#ifndef _FUNCTION_POINTERS_H_
-#define _FUNCTION_POINTERS_H_
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - describes the sweet boy or girl
+ * @name: name of dog
+ * @age: how old is that dog?
+ * @owner: who is the lucky person that gets to live with that dog?
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ * typedef dog_t - defines a new name for type struct dog
+ */
+typedef struct dog dog_t;
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
