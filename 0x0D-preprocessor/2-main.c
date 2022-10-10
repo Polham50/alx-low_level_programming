a7425b2ba9893909d55fbb1d2235301c407b904c
Timothy Oladapo <90522458+Polham50@users.noreply.github.com>
2022-10-11 00:56:41 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,11 @@
-dkfajdf
+#include <stdio.h>
+/**
+ * main - prints the name of the file it was compiled from
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
