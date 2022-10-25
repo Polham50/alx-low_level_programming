252437b78b9f12ecbab26847f05807548b30175f
Timothy Oladapo <90522458+Polham50@users.noreply.github.com>
2022-10-25 23:18:19 +0100

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,57 +1,43 @@
-/*
- * File: 102-free_listint_safe.c
- * Auth: Brennan D Baraban
- */
-
-#include "lists.h"
-
-size_t looped_listint_count(listint_t *head);
-size_t free_listint_safe(listint_t **h);
-
 /**
- * looped_listint_count - Counts the number of unique nodes
- *                      in a looped listint_t linked list.
- * @head: A pointer to the head of the listint_t to check.
+ * free_listint_safe - Frees a listint_t list safely (ie.
+ *                     can free lists containing loops)
+ * @h: A pointer to the address of
+ *     the head of the listint_t list.
  *
- * Return: If the list is not looped - 0.
- *         Otherwise - the number of unique nodes in the list.
+ * Return: The size of the list that was freed.
+ *
+ * Description: The function sets the head to NULL.
  */
-size_t looped_listint_count(listint_t *head)
+size_t free_listint_safe(listint_t **h)
 {
-	listint_t *tortoise, *hare;
-	size_t nodes = 1;
+	listint_t *tmp;
+	size_t nodes, index;
 
-	if (head == NULL || head->next == NULL)
-		return (0);
+	nodes = looped_listint_count(*h);
 
-	tortoise = head->next;
-	hare = (head->next)->next;
-
-	while (hare)
+	if (nodes == 0)
 	{
-		if (tortoise == hare)
+		for (; h != NULL && *h != NULL; nodes++)
 		{
-			tortoise = head;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
-
-			tortoise = tortoise->next;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-			}
-
-			return (nodes);
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
 		}
-
-		tortoise = tortoise->next;
-		hare = (hare->next)->next;
 	}
 
-	return (0);
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+
+		*h = NULL;
+	}
+
+	h = NULL;
+
+	return (nodes);
 }
