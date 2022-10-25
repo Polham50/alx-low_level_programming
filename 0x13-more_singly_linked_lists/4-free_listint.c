769de133cb58c99abf640976e1e23514eac7ed6e
Timothy Oladapo <90522458+Polham50@users.noreply.github.com>
2022-10-25 23:01:00 +0100

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,1 +1,19 @@
-akdfa
+#include "lists.h"
+
+/**
+ * free_listint - frees a list
+ * @head: the head of the list
+ * Return: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
