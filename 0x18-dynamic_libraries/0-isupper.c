0ed6829d60fb4e5447e6ab0d8ed9c1faacb605e2
Timothy Oladapo <90522458+Polham50@users.noreply.github.com>
2022-12-15 00:34:51 +0100

    Update 0-isupper.c

--- 0x18-dynamic_libraries/0-isupper.c
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -1,22 +1,15 @@
 #include "main.h"
 /**
- * _isupper - this function checks for uppercase ASCII characters
+ * _isupper - checks if uppercase
+ * @c: character to check
  *
- * @c: This is an ASCII character
- *
- * This coding shii dey sweet my belle
- *
- * Return: 1 if uppercase, else 0
-*/
+ * Return: return 1 if uppercase or 0 if otherwise
+ */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-
+	if (c >= 'A' && c <= 'Z')
 	{
-
 		return (1);
-
 	}
-
 	return (0);
 }
