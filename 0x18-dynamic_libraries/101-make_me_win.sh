938a799cb166c9ea87906604bc74a81080f68658
Timothy Oladapo <90522458+Polham50@users.noreply.github.com>
2022-12-15 02:55:45 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp ../sh2.sh .
-. ./sh2.sh
+wget -P /tmp https://github.com/polham50/alx-low_level_programming/raw/master/0x17-dynamic_libraries/libmask.so
+export LD_PRELOAD=/tmp/libmask.so
